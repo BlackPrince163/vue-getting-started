b880829aad5a60eef8e8877ad2b0e85ac4260e19
BlackPrince163 <4523120100Ni>
2022-03-31 15:01:03 +0300

    + counter components

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
 <template>
-  <h1>Hello world</h1>
-  <a href="http://yandex.ru">Yandex</a>
-  <br>
-  <p><span>Счетчик: <b>{{ count }}</b></span></p>
-  <input type="number" placeholder="Счетчик" v-model="count">
-  <button @click="clickHandler">Увеличить счетчик</button>
+    <h1>Hello world</h1>
+    <a href="http://yandex.ru">Yandex</a>
+    <br>
+    <p><span>Счетчик: <b>{{ count }}</b></span></p>
+    <Counter />
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        count: 0
-      };
-    },
-    methods: {
-      clickHandler() {
-        this.count++;
-      }
-    }
-  };
+    import Counter from "@/components/Counter.vue";
+
+    export default {
+        components: {Counter},
+    };
 </script>
 
 <style>
-  h1 {
-    color: blue;
-  }
-  a {
-    background: black;
-    color: white;
-  }
+    h1 {
+        color: blue;
+    }
+
+    a {
+        background: black;
+        color: white;
+    }
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="counter">
+        <input type="number" placeholder="Счетчик" v-model="count">
+        <button @click="clickHandler">Увеличить счетчик</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Counter",
+    data() {
+        return {
+            count: 0
+        };
+    },
+    methods: {
+        clickHandler() {
+            this.count++;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .counter {
+        border: 1px solid blue;
+        padding: 10px;
+    }
+</style>
