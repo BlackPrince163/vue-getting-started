f1b069bad98326f14a95354c576742dc88566e79
BlackPrince163 <4523120100Ni>
2022-03-31 13:32:23 +0300

    + simple component with count variable

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
   <h1>Hello world</h1>
+  <a href="http://yandex.ru">Yandex</a>
+  <br>
+  <p><span>Счетчик: <b>{{ count }}</b></span></p>
+  <input type="number" placeholder="Счетчик">
+  <button>Увеличить счетчик</button>
 </template>
 
-<style>
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      };
+    }
+  };
+</script>
 
+<style>
+  h1 {
+    color: blue;
+  }
+  a {
+    background: black;
+    color: white;
+  }
 </style>
